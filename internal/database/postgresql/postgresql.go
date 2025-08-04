d8fe64037b7f8846c373c7fc1bb0edab585a7b4a
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-08-03 21:41:09 -0300

    chore: add loggers at postgresql.go

--- internal/database/postgresql/postgresql.go
+++ internal/database/postgresql/postgresql.go
@@ -13,6 +13,9 @@ import (
 var db *sql.DB
 
 func Connection() (*sql.DB, error) {
+	logger.Info("Init Connection with Postgres",
+		zap.String("journey", "connectionToPostgres"),
+	)
 	if db == nil {
 		var err error
 		db, err = sql.Open("postgres", getPostgresDSN())
@@ -30,22 +33,38 @@ func Connection() (*sql.DB, error) {
 			return nil, err
 		}
 	}
+
+	logger.Info("Connection with Postgres executed successfully",
+		zap.String("journey", "connectionToPostgres"),
+	)
 	return db, nil
 }
 
 func CloseConnection() {
+	logger.Info("Init CloseConnection with Postgres",
+		zap.String("journey", "closeConnectionToPostgres"),
+	)
 	if db != nil {
 		_ = db.Close()
 	}
+	logger.Info("CloseConnection with Postgres executed successfully",
+		zap.String("journey", "closeConnectionToPostgres"),
+	)
 }
 
 func getPostgresDSN() string {
+	logger.Info("Init GetPostgresDSN",
+		zap.String("journey", "connectionToPostgres"),
+	)
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	dbname := os.Getenv("DB_NAME")
 
+	logger.Info("GetPostgresDSN executed successfully",
+		zap.String("journey", "connectionToPostgres"),
+	)
 	// Exemplo: "host=localhost port=5432 user=root password=root dbname=api-hotel-go sslmode=disable"
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, pass, dbname)
