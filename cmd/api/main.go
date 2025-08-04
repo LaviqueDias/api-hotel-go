ead689501470e6d7f2f8bd81ad325495a5d36911
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-08-03 21:23:47 -0300

    chore: create connection with postgres and test in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/LaviqueDias/api-hotel-go/internal/configuration/logger"
+	"github.com/LaviqueDias/api-hotel-go/internal/database/postgresql"
+)
 
 func main() {
+	logger.Info("About to start user application")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Olá, mundo")
+	db, err := postgresql.Connection()
+	if err != nil {
+		log.Fatal("Err to connect to bd", err)
+	}
+	defer postgresql.CloseConnection()
+
+	p := db.Ping()
+	fmt.Println(p)
 }
---
+++ internal/database/postgresql/postgresql.go
@@ -0,0 +1,54 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/LaviqueDias/api-hotel-go/internal/configuration/logger"
+	_ "github.com/lib/pq"
+	"go.uber.org/zap"
+)
+
+var db *sql.DB
+
+func Connection() (*sql.DB, error) {
+	if db == nil {
+		var err error
+		db, err = sql.Open("postgres", getPostgresDSN())
+		if err != nil {
+			return nil, err
+		}
+
+		// Ajustar o timezone da sessão (não trava a aplicação se falhar)
+		if _, err := db.Exec("SET TIME ZONE 'America/Sao_Paulo'"); err != nil {
+			logger.Error("warning: não foi possível ajustar time zone da sessão", err,
+				zap.String("journey", "connectionToPostgres"))
+		}
+
+		if err := db.Ping(); err != nil {
+			return nil, err
+		}
+	}
+	return db, nil
+}
+
+func CloseConnection() {
+	if db != nil {
+		_ = db.Close()
+	}
+}
+
+func getPostgresDSN() string {
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	// Exemplo: "host=localhost port=5432 user=root password=root dbname=api-hotel-go sslmode=disable"
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, pass, dbname)
+}
+
+
