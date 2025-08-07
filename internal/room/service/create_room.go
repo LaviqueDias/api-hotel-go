b1efb6d8473e37c8263038e46d3dd204f1247da6
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-08-07 13:52:25 -0300

    todo at create_room service

--- internal/room/service/create_room.go
+++ internal/room/service/create_room.go
@@ -11,7 +11,12 @@ func (rs *roomServiceInterface) CreateRoom(roomDTO *model.RoomDTO) (*model.RoomD
 	logger.Info("Init Createroom service",
 		zap.String("journey", "Createroom"),
 	)
-	
+
+	// pegar o hotel pelo ID - tenho que acessar o repository de ontem pra usar a f unção GetHotelByID, mas não consigo pq é uma interface
+	// depois, verificar se hotel existe
+	// depois, fazer hotel.AddRoom(room) - tenho que acessar o model de hotel pra usar a função AddRoom
+	// depois dou continuidade ao codigo que já está pronto
+
 	roomDTO, err := rs.repository.CreateRoom(roomDTO)
 	if err != nil {
 		logger.Error("Error trying to call repository",
