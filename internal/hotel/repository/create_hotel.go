bf6ed4606771b5bf1e51938424b27d8a21d91673
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-08-07 12:31:23 -0300

    Refactor: I was using DTO to send to DB. So, I do DTO > hotel - send to DB - hotel > DTO

--- internal/hotel/repository/create_hotel.go
+++ internal/hotel/repository/create_hotel.go
@@ -12,12 +12,14 @@ func (hr *hotelRepositoryInterface) CreateHotel(hotelDTO *model.HotelDTO) (*mode
 		zap.String("journey", "CreateHotel"),
 	)
 
+	hotel := model.HotelDTOToHotel(hotelDTO)
+
 	query := `INSERT INTO hotels (name) 
               VALUES ($1)
               RETURNING id`
 
 	var lastInsertID int
-	err := hr.databaseConnection.QueryRow(query, hotelDTO.Name).Scan(&lastInsertID)
+	err := hr.databaseConnection.QueryRow(query, hotel.Name).Scan(&lastInsertID)
 	if err != nil {
 		logger.Error("Error trying to insert hotel",
 			err,
@@ -26,7 +28,9 @@ func (hr *hotelRepositoryInterface) CreateHotel(hotelDTO *model.HotelDTO) (*mode
 		return nil, rest_err.NewInternalServerError(err.Error())
 	}
 
-	hotelDTO.ID = lastInsertID
+	hotel.ID = lastInsertID
+
+	hotelDTO = model.HotelToHotelDTO(hotel)
 
 	logger.Info("Hotel inserted successfully",
 		zap.Int("hotelId", lastInsertID),
