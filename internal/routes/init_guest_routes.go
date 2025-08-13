cd9590e197b4ada0caedeaf3efa795c79b21d088
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-08-13 08:30:30 -0300

    feat: creat routes to guest

---
+++ internal/routes/init_guest_routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/LaviqueDias/api-hotel-go/internal/guest/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitGuestRoutes(r *gin.RouterGroup, guestController controller.GuestControllerInterface) {
+
+	r.POST("/", guestController.CreateGuest)
+	// r.GET("/list", guestController.Listguests)
+	// r.GET("/id/:id", guestController.GetguestByID)
+	// r.PUT("/:id", guestController.Updateguest)
+	// r.DELETE("/:id", guestController.Deleteguest)
+
+}
